ba15dad7b71b3fe01d651f2a4a130dd4cd217e2a
ganesh shinde <ganeshss978081@gmail.com>
2024-08-20 15:23:55 +0530

    distributer client api ameded

--- app/src/main/java/com/ltrsoft/andromotapp/apimodelclasses/Distributer_Client_api.java
+++ app/src/main/java/com/ltrsoft/andromotapp/apimodelclasses/Distributer_Client_api.java
@@ -1,6 +1,5 @@
 package com.ltrsoft.andromotapp.apimodelclasses;
 
-import com.ltrsoft.andromotapp.pojoclasses.Cock_List;
 import com.ltrsoft.andromotapp.pojoclasses.Distributer_Client;
 
 import java.util.List;
@@ -11,30 +10,34 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
-public interface Distributer_Client_api {
-    @FormUrlEncoded
-    @POST("create_distributer_client.php")
-    Call<Distributer_Client> create_Distributer_Client(
-            @Field("distributer_id") String distributer_id ,
-            @Field("client_id") String  client_id
-    );
-    @FormUrlEncoded
-    @POST("update_distributer_client.php")
-    Call<Distributer_Client> update_Distributer_Client(
-            @Field("distributer_id") String distributer_id ,
-            @Field("client_id") String  client_id
-    );
-    @GET("read_distributer_client.php")
-    Call<Distributer_Client> read_by_id_Distributer_Client(
-            @Field("distributer_id") String distributer_id
-    );
+public interface distributer_client_api {
 
-    @GET("readall_distributer_client.php")
-    Call<List<Distributer_Client>> readall_Distributer_Client();
+        @FormUrlEncoded
+        @POST("create_distributer_client.php")
+        Call<Distributer_Client> create_Distributer_Client(
+                @Field("distributer_id") String distributer_id ,
+                @Field("client_id") String  client_id
+        );
+        @FormUrlEncoded
+        @POST("update_distributer_client.php")
+        Call<Distributer_Client> update_Distributer_Client(
+                @Field("distributer_id") String distributer_id ,
+                @Field("client_id") String  client_id
+        );
+        @GET("read_distributer_client.php")
+        Call<Distributer_Client> read_by_id_Distributer_Client(
+                @Field("distributer_id") String distributer_id
+        );
 
-    @POST("delete_distributer_client.php")
-    Call<Distributer_Client> delete_Distributer_Client(
-            @Field("distributer_id") String distributer_id
-    );
-}
+        @GET("readall_distributer_client.php")
+        Call<List<Distributer_Client>> readall_Distributer_Client();
+
+        @POST("delete_distributer_client.php")
+        Call<Distributer_Client> delete_Distributer_Client(
+                @Field("distributer_id") String distributer_id
+        );
+    }
+
+
+
 
